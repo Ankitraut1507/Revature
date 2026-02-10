c86b0e092d48144cdbf71cbbf8d89347022b3da6
Ankit <ankitraut1507@gmail.com>
2026-02-10 09:39:03 +0530

    Initial commit - CSharp Assignments

---
+++ CSharpAssignments/ControlFlowDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignments
+{
+    internal class ControlFlowDemo
+    {
+        public static void Run()
+        {
+            int number = 10;
+
+            if (number > 0)
+                Console.WriteLine("Positive number");
+            else
+                Console.WriteLine("Negative number");
+        }
+    }
+}
---
+++ CSharpAssignments/LoopDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignments
+{
+    internal class LoopDemo
+    {
+        public static void Run()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
