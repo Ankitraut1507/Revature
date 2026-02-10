c212e8cf35c290d68ce7e81c753451b2de34dc15
Ankit <ankitraut1507@gmail.com>
2026-02-10 14:26:23 +0530

    second commit - CSharp Assignments

---
+++ DataTypesDemo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAssignments
+{
+    internal class DataTypesDemo
+    {
+        public static void Run()
+        {
+            int age = 22;
+            double marks = 85.5;
+            bool isPassed = true;
+            char grade = 'A';
+
+            Console.WriteLine("Age: " + age);
+            Console.WriteLine("Marks: " + marks);
+            Console.WriteLine("Passed: " + isPassed);
+            Console.WriteLine("Grade: " + grade);
+        }
+    }
+}
